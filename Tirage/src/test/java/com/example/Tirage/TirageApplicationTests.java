871eefe8a5d271c115ca3f1f91ac076858e180c6
dsekou166 <dsekou166@gmail.com>
2022-08-23 09:09:35 +0000

    news project

---
+++ Tirage/src/test/java/com/example/Tirage/TirageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tirage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TirageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
